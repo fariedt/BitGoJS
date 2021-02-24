fcd62ab7ca4edc29909cc992afe5418c59a1615e
Faried Nawaz <faried@tintash.com>
2021-02-24 17:47:21 +0500

    feat(account-lib): stacks keyPair.ts uses HDNode

--- modules/account-lib/src/coin/stacks/keyPair.ts
+++ modules/account-lib/src/coin/stacks/keyPair.ts
@@ -1,21 +1,29 @@
+import { randomBytes } from 'crypto';
+import { HDNode } from '@bitgo/utxo-lib';
 import { publicKeyToAddress } from '@stacks/encryption';
-import { DefaultKeys, isPrivateKey, isPublicKey, KeyPairOptions } from '../baseCoin/iface';
+import { DefaultKeys, isPrivateKey, isPublicKey, isSeed, KeyPairOptions } from '../baseCoin/iface';
 import { Secp256k1ExtendedKeyPair } from '../baseCoin/secp256k1ExtendedKeyPair';
 import { hexPrefixString } from './utils';
+const DEFAULT_SEED_SIZE_BYTES = 64;
 
 export class KeyPair extends Secp256k1ExtendedKeyPair {
   constructor(source?: KeyPairOptions) {
     super(source);
-    if (source) {
-      if (isPrivateKey(source)) {
-        super.recordKeysFromPrivateKey(source.prv);
-      } else if (isPublicKey(source)) {
-        super.recordKeysFromPublicKey(source.pub);
-      } else {
-        throw new Error('Invalid key pair options');
-      }
+    if (!source) {
+      const seed = randomBytes(DEFAULT_SEED_SIZE_BYTES);
+      this.hdNode = HDNode.fromSeedBuffer(seed);
+    } else if (isSeed(source)) {
+      this.hdNode = HDNode.fromSeedBuffer(source.seed);
+    } else if (isPrivateKey(source)) {
+      super.recordKeysFromPrivateKey(source.prv);
+    } else if (isPublicKey(source)) {
+      super.recordKeysFromPublicKey(source.pub);
     } else {
       throw new Error('Invalid key pair options');
+    }
+
+    if (this.hdNode) {
+      this.keyPair = this.hdNode.keyPair;
     }
   }
 
