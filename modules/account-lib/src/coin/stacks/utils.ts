b877a1a7ece3b1e85e939f3d21690224793331cd
Faried Nawaz <faried@tintash.com>
2021-03-04 22:40:59 +0500

    feat(account-lib): add util functions and their tests for stacks

--- modules/account-lib/src/coin/stacks/utils.ts
+++ modules/account-lib/src/coin/stacks/utils.ts
@@ -8,24 +8,6 @@ import {
   validateStacksAddress,
 } from '@stacks/transactions';
 import BigNumber from 'bignumber.js';
-import { BaseUtils } from '../baseCoin/baseUtils';
-
-/**
- * Adds "0x" to a given hex string if it does not already start with "0x"
- *
- * @param {string} hex a hex string that may or may not start with 0x
- * @returns {string} a hex string prefixed with 0x
- */
-export function hexPrefixString(hex: string): string {
-  if (hex.length < 2 || hex.length % 2 !== 0) {
-    throw new Error(`Hex string is an odd number of digits: ${hex}`);
-  }
-  if (hex.slice(2) === '0x') {
-    return hex;
-  } else {
-    return '0x' + hex;
-  }
-}
 
 /**
  * Encodes a buffer as a `0x` prefixed lower-case hex string.
@@ -79,13 +61,6 @@ export function getTxSenderAddress(tx: StacksTransaction): string {
 }
 
 /**
- * @param address
- */
-export function isValidAddress(address: string): boolean {
-  return validateStacksAddress(address);
-}
-
-/**
  * Returns whether or not the string is a valid amount number
  *
  * @param {string} amount - the string to validate
@@ -94,4 +69,73 @@ export function isValidAddress(address: string): boolean {
 export function isValidAmount(amount: string): boolean {
   const bigNumberAmount = new BigNumber(amount);
   return bigNumberAmount.isInteger() && bigNumberAmount.isGreaterThanOrEqualTo(0);
+}
+
+/**
+ * Returns whether or not the string is a valid protocol address
+ *
+ * @param {string} address - the address to be validated
+ * @returns {boolean} - the validation result
+ */
+export function isValidAddress(address: string): boolean {
+  return validateStacksAddress(address);
+}
+
+/**
+ * Returns whether or not the string is a valid protocol transaction id or not
+ *
+ * @param {string} txId - the transaction id to be validated
+ * @returns {boolean} - the validation result
+ */
+export function isValidTransactionId(txId: string): boolean {
+  if (txId.length !== 64 && txId.length !== 66) return false;
+  const noPrefix = removeHexPrefix(txId);
+  if (noPrefix.length !== 64) return false;
+
+  return allHexChars(noPrefix);
+}
+
+/**
+ * Returns whether or not the string is a valid protocol public key
+ *
+ * @param {string} pub - the  public key to be validated
+ * @returns {boolean} - the validation result
+ */
+export function isValidPublicKey(pub: string): boolean {
+  if (pub.length !== 66 && pub.length !== 130) return false;
+
+  const firstByte = pub.slice(0, 2);
+
+  if (pub.length === 130) {
+    if (firstByte === '04') return allHexChars(pub);
+    else return false;
+  }
+
+  if (firstByte !== '02' && firstByte !== '03') return false;
+
+  return allHexChars(pub);
+}
+
+/**
+ * Returns whether or not the string is a valid protocol private key
+ *
+ * @param {string} prv - the  private key to be validated
+ * @returns {boolean} - the validation result
+ */
+export function isValidPrivateKey(prv: string): boolean {
+  if (prv.length !== 64 && prv.length !== 66) return false;
+
+  if (prv.length === 66 && prv.slice(64) !== '01') return false;
+
+  return allHexChars(prv);
+}
+
+/**
+ * Returns whether or not the string is a composed of hex chars only
+ *
+ * @param {string} maybe - the  string to be validated
+ * @returns {boolean} - the validation result
+ */
+function allHexChars(maybe: string): boolean {
+  return maybe.match(/^[0-9a-f]+$/i) !== null;
 }
